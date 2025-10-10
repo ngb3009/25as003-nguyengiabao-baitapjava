44e3d98cdbc774231c6e490f69f2a4221e2ca822
ngb3009 <ngb3009@gmail.com>
2025-10-10 09:10:46 +0700

    Commit ex1.8

---
+++ src/ex18/Main18.java
@@ -0,0 +1,25 @@
+package ex18;
+
+public class Main18 {
+    public static void main(String[] args) {
+        Time t1 = new Time(1, 2, 3);
+        System.out.println(t1);
+
+        t1.setHour(4);
+        t1.setMinute(5);
+        t1.setSecond(6);
+        System.out.println(t1);
+        System.out.println("Hour: " + t1.getHour());
+        System.out.println("Minute: " + t1.getMinute());
+        System.out.println("Second: " + t1.getSecond());
+
+        t1.setTime(9, 37, 58);
+        System.out.println(t1);
+
+        System.out.println(t1.nextSecond());
+        System.out.println(t1.nextSecond().nextSecond());
+
+        System.out.println(t1.previousSecond());
+        System.out.println(t1.previousSecond().previousSecond());
+    }
+}
